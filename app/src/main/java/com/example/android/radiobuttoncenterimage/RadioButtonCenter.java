4c98c7564523c612170d4b1fae7aef473817b77f
Elorri <etchemendy.elorri@gmail.com>
2016-09-04 20:54:48 +0200

    Try decoding the drawable into a bitmap and draw it on canavas

--- app/src/main/java/com/example/android/radiobuttoncenterimage/RadioButtonCenter.java
+++ app/src/main/java/com/example/android/radiobuttoncenterimage/RadioButtonCenter.java
@@ -3,52 +3,72 @@
 package com.example.android.radiobuttoncenterimage;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.view.Gravity;
+import android.util.Log;
 import android.widget.RadioButton;
 
-public class RadioButtonCenter extends RadioButton
-{
-  Drawable buttonDrawable;
+public class RadioButtonCenter extends RadioButton {
+    Drawable buttonDrawable;
+    private Bitmap bitmap;
 
-  public RadioButtonCenter(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    Resources resources=getResources();
-    TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RadioButtonCenter, 0, 0);
-    buttonDrawable = resources.getDrawable(a.getInteger(R.styleable.RadioButtonCenter_drawable, 0));
-    setButtonDrawable(android.R.color.transparent);
-  }
+    public RadioButtonCenter(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RadioButtonCenter, 0, 0);
+        buttonDrawable = a.getDrawable(R.styleable.RadioButtonCenter_drawable);
 
+        bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_pen);
 
-
-  @Override
-  protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-
-    if (buttonDrawable != null) {
-      buttonDrawable.setState(getDrawableState());
-      final int verticalGravity = getGravity() & Gravity.VERTICAL_GRAVITY_MASK;
-      final int height = buttonDrawable.getIntrinsicHeight();
-
-      int y = 0;
-
-      switch (verticalGravity) {
-        case Gravity.BOTTOM:
-          y = getHeight() - height;
-          break;
-        case Gravity.CENTER_VERTICAL:
-          y = (getHeight() - height) / 2;
-          break;
-      }
-
-      int buttonWidth = buttonDrawable.getIntrinsicWidth();
-      int buttonLeft = (getWidth() - buttonWidth) / 2;
-      buttonDrawable.setBounds(buttonLeft, y, buttonLeft+buttonWidth, y + height);
-      buttonDrawable.draw(canvas);
     }
-  }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//
+//        if (buttonDrawable != null) {
+//            Log.e("Nebo", Thread.currentThread().getStackTrace()[2]+"getDrawableState() " +
+//                    ""+getDrawableState());
+//            buttonDrawable.setState(getDrawableState());
+//            final int verticalGravity = getGravity() & Gravity.VERTICAL_GRAVITY_MASK;
+//            final int height = buttonDrawable.getIntrinsicHeight();
+//
+//            int y = 0;
+//
+//            switch (verticalGravity) {
+//                case Gravity.BOTTOM:
+//                    y = getHeight() - height;
+//                    break;
+//                case Gravity.CENTER_VERTICAL:
+//                    y = (getHeight() - height) / 2;
+//                    break;
+//            }
+//
+//            int buttonWidth = buttonDrawable.getIntrinsicWidth();
+//            int buttonLeft = (getWidth() - buttonWidth) / 2;
+//            buttonDrawable.setBounds(buttonLeft, y, buttonLeft + buttonWidth, y + height);
+//            buttonDrawable.draw(canvas);
+
+
+//        if(isChecked()){
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setAntiAlias(true);
+//            Drawable drawable=getResources().getDrawable(R.drawable.ic_pen);
+//            drawable.draw(canvas);
+
+        Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "bitmap" + bitmap);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        //      }else{
+
+
+        //    }
+    }
 }
