acab9b6204e9ae3b6ee04811a9d5d3864535bffe
Elorri <etchemendy.elorri@gmail.com>
2016-09-06 23:14:07 +0200

    Working but without onMeasure extra padding is added and we got rect istead of square

--- app/src/main/java/com/example/android/radiobuttoncenterimage/MainActivity.java
+++ app/src/main/java/com/example/android/radiobuttoncenterimage/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.android.radiobuttoncenterimage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view= findViewById(R.id.radio1);
+
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("Nebo", Thread.currentThread().getStackTrace()[2]
+                        + " height " + view.getHeight()
+                        + " - measured height " + view.getMeasuredHeight()
+                        + " - width " +view.getWidth()
+                        + " - measured width " +view.getMeasuredWidth()
+                );
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("Nebo", Thread.currentThread().getStackTrace()[2]
+                        + " height " + view.getHeight()
+                        + " - measured height " + view.getMeasuredHeight()
+                        + " - width " +view.getWidth()
+                        + " - measured width " +view.getMeasuredWidth()
+                );
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/radiobuttoncenterimage/RadioButtonCenter.java
+++ app/src/main/java/com/example/android/radiobuttoncenterimage/RadioButtonCenter.java
@@ -4,71 +4,87 @@ package com.example.android.radiobuttoncenterimage;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.RadioButton;
 
 public class RadioButtonCenter extends RadioButton {
     Drawable buttonDrawable;
-    private Bitmap bitmap;
+
 
     public RadioButtonCenter(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.RadioButtonCenter, 0, 0);
         buttonDrawable = a.getDrawable(R.styleable.RadioButtonCenter_drawable);
 
-        bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_pen);
-
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
+       // super.onDraw(canvas);
+
+        if (buttonDrawable != null) {
+            buttonDrawable.setState(getDrawableState());
+            final int verticalGravity = getGravity() & Gravity.VERTICAL_GRAVITY_MASK;
+            final int height = buttonDrawable.getIntrinsicHeight();
+
+            Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "getHeight() " + getHeight()
+                    + "getWidth() " + getWidth());
+
+            Log.e("Nebo", Thread.currentThread().getStackTrace()[2]
+                    + "height " + buttonDrawable.getIntrinsicHeight()
+                    + "width " + buttonDrawable.getIntrinsicWidth());
+
+            int y = 0;
+
+            switch (verticalGravity) {
+                case Gravity.BOTTOM:
+                    y = getHeight() - height;
+                    break;
+                case Gravity.CENTER_VERTICAL:
+                    y = (getHeight() - height) / 2;
+                    break;
+            }
+
+            int buttonWidth = buttonDrawable.getIntrinsicWidth();
+            int buttonLeft = (getWidth() - buttonWidth) / 2;
+            buttonDrawable.setBounds(buttonLeft, y, buttonLeft + buttonWidth, y + height);
+            buttonDrawable.draw(canvas);
+
+
+////        if(isChecked()){
 //
-//        if (buttonDrawable != null) {
-//            Log.e("Nebo", Thread.currentThread().getStackTrace()[2]+"getDrawableState() " +
-//                    ""+getDrawableState());
-//            buttonDrawable.setState(getDrawableState());
-//            final int verticalGravity = getGravity() & Gravity.VERTICAL_GRAVITY_MASK;
-//            final int height = buttonDrawable.getIntrinsicHeight();
+//        Paint paint = new Paint();
+//        paint.setColor(Color.RED);
+//        paint.setAntiAlias(true);
+////            Drawable drawable=getResources().getDrawable(R.drawable.ic_pen);
+////            drawable.draw(canvas);
 //
-//            int y = 0;
+//        Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "bitmap" + bitmap);
+//        canvas.drawBitmap(bitmap, 0, 0, null);
+//        //      }else{
 //
-//            switch (verticalGravity) {
-//                case Gravity.BOTTOM:
-//                    y = getHeight() - height;
-//                    break;
-//                case Gravity.CENTER_VERTICAL:
-//                    y = (getHeight() - height) / 2;
-//                    break;
-//            }
 //
-//            int buttonWidth = buttonDrawable.getIntrinsicWidth();
-//            int buttonLeft = (getWidth() - buttonWidth) / 2;
-//            buttonDrawable.setBounds(buttonLeft, y, buttonLeft + buttonWidth, y + height);
-//            buttonDrawable.draw(canvas);
+//        //    }
+        }
 
-
-//        if(isChecked()){
-
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setAntiAlias(true);
-//            Drawable drawable=getResources().getDrawable(R.drawable.ic_pen);
-//            drawable.draw(canvas);
-
-        Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "bitmap" + bitmap);
-        canvas.drawBitmap(bitmap, 0, 0, null);
-        //      }else{
-
-
-        //    }
     }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(
+                MeasureSpec.makeMeasureSpec(buttonDrawable.getIntrinsicWidth(), MeasureSpec.EXACTLY),
+                MeasureSpec.makeMeasureSpec(buttonDrawable.getIntrinsicHeight(), MeasureSpec.EXACTLY));
+
+//        super.onMeasure(
+//                MeasureSpec.makeMeasureSpec(buttonDrawable.getBounds().width(), MeasureSpec.EXACTLY),
+//                MeasureSpec.makeMeasureSpec(buttonDrawable.getBounds().height(), MeasureSpec.EXACTLY));
+    }
+
+
+
 }
