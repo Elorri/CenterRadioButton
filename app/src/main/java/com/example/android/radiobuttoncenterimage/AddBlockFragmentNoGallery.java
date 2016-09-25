0b0a21796978335584f2360f2d8d76e525c1e386
Elorri <etchemendy.elorri@gmail.com>
2016-09-25 14:19:04 +0200

    AddBlockFragment is tidied

--- app/src/main/java/com/example/android/radiobuttoncenterimage/AddBlockFragmentNoGallery.java
+++ app/src/main/java/com/example/android/radiobuttoncenterimage/AddBlockFragmentNoGallery.java
@@ -1,21 +1,18 @@
 package com.example.android.radiobuttoncenterimage;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,9 +21,7 @@ import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 
 /**
  * Created by Elorri on 25/09/2016.
@@ -42,20 +37,19 @@ public class AddBlockFragmentNoGallery extends Fragment implements View.OnClickL
     private static final int CAPTURE_IMAGE_REQUEST = 1;
     private static final int PICK_IMAGE_REQUEST = 2;
 
+    private Activity mActivity;
+    private Context mContext;
     private Resources mResources;
     private Callback mCallback;
-    private Context mContext;
 
-    //Saved as member variable in onClick->requestImageCapture and used in onActivityForResult
+    //Saved as member variable in onClick->requestImageCapture and used in onActivityForResult->createObjectBlock
     private File mCaptureImageFile;
     private String mCaptureImageFileName;
     private String mCaptureImageMimeType;
 
-    //Saved as member variable in onClick->requestImagePick and used in onActivityForResult
-    private File mPickImageFile;
+    //Saved as member variable in onActivityForResult->getImageNameAndMimetype and used in onActivityForResult->createObjectBlock
     private String mPickImageFileName;
     private String mPickImageMimeType;
-
 
     //Use by EditingFragment
     public interface Callback {
@@ -73,17 +67,16 @@ public class AddBlockFragmentNoGallery extends Fragment implements View.OnClickL
         void addBlock(Bitmap bitmap);
     }
 
-
     public void setCallback(Callback callback) {
         mCallback = callback;
     }
-
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = getContext();
         mResources = mContext.getResources();
+        mActivity = getActivity();
     }
 
     @Nullable
@@ -95,7 +88,6 @@ public class AddBlockFragmentNoGallery extends Fragment implements View.OnClickL
         addBlockView.setAddBlockFragment(this);
         return view;
     }
-
 
     public void onClick(View view) {
         mCallback.closeWindowsPopup();
@@ -124,75 +116,36 @@ public class AddBlockFragmentNoGallery extends Fragment implements View.OnClickL
     private void requestImageCapture() {
         String intentName = MediaStore.ACTION_IMAGE_CAPTURE;
 
-        checkAndAskForPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        PlatformUtils.checkAndAskForPermission(mActivity, android.Manifest.permission
+                .WRITE_EXTERNAL_STORAGE);
         Intent intent = new Intent(intentName);
-        if (intent.resolveActivity(getActivity().getPackageManager()) == null) {
+        if (intent.resolveActivity(mActivity.getPackageManager()) == null) {
             Toast.makeText(mContext, mResources.getString(R.string.no_take_picture_app_installed), Toast.LENGTH_SHORT).show();
             return;
         }
+        //Create a file where we will store the capture result
+        mCaptureImageFileName = FileUtils.createImageFileName(mResources);
+        mCaptureImageMimeType = getResources().getString(R.string.default_image_mimetype);
+        //We create the file in Gallery directory because the camera app does not have access to our private cache. We will later create a new file from the resulted bitmap and store it in our app local cache.
         try {
-            //Create a file where we will store the capture result
-            mCaptureImageFileName = createFileName();
-            mCaptureImageMimeType = getResources().getString(R.string.default_image_mimetype);
-            //We create the file in Gallery directory because the camera app does not have access to our private cache. We will later create a new file from the resulted bitmap and store it in our app local cache.
-            mCaptureImageFile = createImageFileInGalleryDirectory(mCaptureImageFileName, mCaptureImageMimeType);
-            Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "mCaptureImageFile before capture path" + Uri.fromFile(mCaptureImageFile));
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mCaptureImageFile));
+            mCaptureImageFile = FileUtils.createImageFileInAppGalleryDirectory(mContext, mCaptureImageFileName, mCaptureImageMimeType);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "mCaptureImageFile before capture path" + Uri.fromFile(mCaptureImageFile));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mCaptureImageFile));
         //This will start the camera capture appropriate app
         startActivityForResult(intent, CAPTURE_IMAGE_REQUEST);
     }
 
-    private String createFileName() {
-        String timeStamp = new SimpleDateFormat(mResources.getString(R.string.date_format)).format(new Date());
-        String imageNamePrefix = mResources.getString(R.string.image_name_prefix);
-        return imageNamePrefix + timeStamp;
-    }
-
-
     private void requestImagePick() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        checkAndAskForPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        PlatformUtils.checkAndAskForPermission(mActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         intent.setType(mResources.getString(R.string.browse_images_input_type));
         //This will start the appropriate app able to pick a file
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
-
-
-    private void checkAndAskForPermission(String permission) {
-        //Before Marshmallow api 23 all permissions were requested at app first installed, asking permission dynamically wasn't possible
-        if (Build.VERSION.SDK_INT < 23) {
-            return;
-        }
-        //The user has already granted the permission we need. We can move on.
-        if (ContextCompat.checkSelfPermission(mContext, permission) == PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        //The user has not granted the permission we need. We ask him to do so.
-        ActivityCompat.requestPermissions(getActivity(), new String[]{permission}, PackageManager.PERMISSION_GRANTED);
-    }
-
-
-    private File createImageFileInGalleryDirectory(String imageFileName, String mimeType) throws IOException {
-        String imageMimeTypePrefix = getResources().getString(R.string.mimetype_image_prefix);
-        String mimeTypeSufix = mimeType.substring(imageMimeTypePrefix.length(), mimeType.length());
-        String imageNameSuffix = mResources.getString(R.string.image_name_suffix, mimeTypeSufix);
-        File cameraDirectoryAlbumNebo = FileUtils.getExternalPictureStorageDirectory(mResources, mResources.getString(R.string.nebo_album));
-        Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "cameraDirectoryAlbumNebo" + cameraDirectoryAlbumNebo);
-        File imageF = File.createTempFile(imageFileName, imageNameSuffix, cameraDirectoryAlbumNebo);
-        return imageF;
-    }
-
-    private File createImageFileInCacheDirectory(String imageFileName, String mimeType) throws IOException {
-        String imageNameSuffix = mResources.getString(R.string.image_name_suffix, MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType));
-        File albumF = mContext.getCacheDir();
-        File imageF = File.createTempFile(imageFileName, imageNameSuffix, albumF);
-        return imageF;
-    }
-
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -200,16 +153,22 @@ public class AddBlockFragmentNoGallery extends Fragment implements View.OnClickL
         // requestCodes with same value in MainActivity and here.
         switch (requestCode) {
             case CAPTURE_IMAGE_REQUEST:
-                if (resultCode == getActivity().RESULT_OK) {
+                if (resultCode == mActivity.RESULT_OK) {
+                    mCaptureImageFile = resizeImageFile(mCaptureImageFile);
+                    File localImageFile = saveGalleryImageFileInLocalCache(mCaptureImageFile, mCaptureImageFileName, mCaptureImageMimeType);
+                    mCaptureImageFile.delete();             //Removed gallery file
+                    mCaptureImageFile = localImageFile;     //Made mCaptureImageFile point to local file
                     addBlockCapturedImage(mCaptureImageFile, mCaptureImageFileName, mCaptureImageMimeType);
                     return;
                 }
                 break;
             case PICK_IMAGE_REQUEST:
-                if (resultCode == getActivity().RESULT_OK) {
+                if (resultCode == mActivity.RESULT_OK) {
                     Uri imageUri = data.getData();
-                    getImageNameMimetypeAndSize(imageUri);
-                    addBlockPickedImage(mPickImageFile, mPickImageFileName, mPickImageMimeType);
+                    getImageNameAndMimetype(imageUri); //Will set mPickImageFileName and mPickImageMimeType
+                    File pickImageFile = saveGalleryImageFileInLocalCache(imageUri, mPickImageFileName, mPickImageMimeType);
+                    pickImageFile = resizeImageFile(pickImageFile);
+                    addBlockPickedImage(pickImageFile, mPickImageFileName, mPickImageMimeType);
                 }
                 break;
             default:
@@ -217,7 +176,38 @@ public class AddBlockFragmentNoGallery extends Fragment implements View.OnClickL
         }
     }
 
-    private void getImageNameMimetypeAndSize(Uri uri) {
+    private File saveGalleryImageFileInLocalCache(File externalImageFile, String filename, String
+            mimeType) {
+        try {
+            Bitmap bitmap = BitmapFactory.decodeFile(externalImageFile.getPath());
+            File localFile = FileUtils.createImageFileInAppCacheDirectory(mContext, filename, mimeType);
+            return FileUtils.saveBitmapInFile(bitmap, localFile, mimeType);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private File saveGalleryImageFileInLocalCache(Uri externalImageUri, String filename, String mimeType) {
+        try {
+            Bitmap pickImageBitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), externalImageUri);
+            File localFile = FileUtils.createImageFileInAppCacheDirectory(mContext, filename, mimeType);
+            return FileUtils.saveBitmapInFile(pickImageBitmap, localFile, mimeType);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private File resizeImageFile(File file) {
+        int targetH = mCallback.getTargetImageViewHeight();
+        int targetW = mCallback.getTargetImageViewWidth();
+        FileUtils.scaleToImageViewSize(file, targetW, targetH);
+        return file;
+    }
+
+
+    private void getImageNameAndMimetype(Uri uri) {
         Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "uri " + uri);
         String[] projection = {MediaStore.MediaColumns.DATA, MediaStore.MediaColumns.DISPLAY_NAME, MediaStore.MediaColumns.MIME_TYPE, MediaStore.MediaColumns.SIZE};
         final int columnPath = 0;
@@ -241,90 +231,18 @@ public class AddBlockFragmentNoGallery extends Fragment implements View.OnClickL
             Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "");
             cursor.close();
         }
-
-        //Because mPickImagePathFile we this turn around
-        try {
-            Bitmap pickImageBitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), uri);
-            Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "pickImageBitmap " + pickImageBitmap);
-            File localFile = createImageFileInCacheDirectory(mPickImageFileName, mPickImageMimeType);
-            mPickImageFile = saveBitmapInFile(pickImageBitmap, localFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
-
-    private File saveBitmapInFile(Bitmap bmp, File file) {
-        OutputStream outStream = null;
-
-        try {
-            outStream = new FileOutputStream(file);
-            bmp.compress(Bitmap.CompressFormat.PNG, 100, outStream);
-            outStream.flush();
-            outStream.close();
-            return file;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
 
     private void addBlockCapturedImage(File file, String filename, String mimeType) {
-        int targetH = mCallback.getTargetImageViewHeight();
-        int targetW = mCallback.getTargetImageViewWidth();
-        String imagePath = file.getPath();
-        Bitmap bitmap = scaleToImageViewSize(imagePath, targetW, targetH);
-        try {
-            File localfile = createImageFileInCacheDirectory(filename, mimeType);
-            mCaptureImageFile = saveBitmapInFile(bitmap, localfile);
-            Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "after capture  mCaptureImageFile path " + Uri.fromFile(mCaptureImageFile));
-            //now that our picture is saved locally, we can delete the public picture
-            Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "to be deleted " + file.toString());
-            file.delete();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "will add to galery  mCaptureImageFile path " + mCaptureImageFile.getPath());
+        Bitmap bitmap = BitmapFactory.decodeFile(file.getPath());
         mCallback.addBlock(bitmap);
         mCallback.createObjectBlock(SKETCH_BACKEND_ID, filename, Uri.fromFile(mCaptureImageFile).toString(), mimeType);
-
     }
-
-
-    private Bitmap scaleToImageViewSize(String photoPath, int targetW, int targetH) {
-        /*This function has been entiery copied from Google Sample PhotoIntentActivity https://developer.android.com/training/camera/photobasics.html*/
-        /* There isn't enough memory to open up more than a couple camera photos */
-        /* So pre-scale the target bitmap into which the file is decoded */
-
-        /* Get the size of the image */
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(photoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-        /* Figure out which way needs to be reduced less */
-        int scaleFactor = 1;
-        if ((targetW > 0) || (targetH > 0)) {
-            scaleFactor = Math.min(photoW / targetW, photoH / targetH);
-        }
-
-        /* Set bitmap options to scale the image decode target */
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-        /* Decode the JPEG file into a Bitmap */
-        return BitmapFactory.decodeFile(photoPath, bmOptions);
-
-    }
-
 
     private void addBlockPickedImage(File pickImageFile, String pickImageFileName, String mimeType) {
-        String imagePath = Uri.fromFile(pickImageFile).toString();
-        mCallback.addBlock(BitmapFactory.decodeFile(imagePath));
-        mCallback.createObjectBlock(SKETCH_BACKEND_ID, pickImageFileName, imagePath, mimeType);
+        String imageUriPath = Uri.fromFile(pickImageFile).toString();
+        mCallback.addBlock(BitmapFactory.decodeFile(imageUriPath));
+        mCallback.createObjectBlock(SKETCH_BACKEND_ID, pickImageFileName, imageUriPath, mimeType);
     }
 
 }
--- app/src/main/java/com/example/android/radiobuttoncenterimage/FileUtils.java
+++ app/src/main/java/com/example/android/radiobuttoncenterimage/FileUtils.java
@@ -3,28 +3,33 @@ package com.example.android.radiobuttoncenterimage;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Environment;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileUtils {
 
 
-    public static File getExternalPictureStorageDirectory(Resources resources, String   appExternalDirectoryName)
-    {
-        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())){
+    public static File getExternalPictureStorageDirectory(Resources resources, String appExternalDirectoryName) {
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
             //External storage is not mounted
             return null;
         }
 
         File storageDir = getPictureDirectory(resources, appExternalDirectoryName);
-        if (storageDir != null)
-        {
-            if (!storageDir.mkdirs())
-            {
-                if (!storageDir.exists())
-                {
+        if (storageDir != null) {
+            if (!storageDir.mkdirs()) {
+                if (!storageDir.exists()) {
                     //failed to create directory
                     return null;
                 }
@@ -34,18 +39,81 @@ public class FileUtils {
     }
 
 
-
-    private static File getPictureDirectory(Resources resources, String albumName)
-    {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO)
-        {
+    private static File getPictureDirectory(Resources resources, String albumName) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO) {
             return new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), albumName);
-        }
-        else
-        {
+        } else {
             // Standard storage location for digital camera files
             final String CAMERA_DIR = resources.getString(R.string.standard_camera_directory);
             return new File(Environment.getExternalStorageDirectory() + CAMERA_DIR + albumName);
         }
     }
+
+
+    public static String createImageFileName(Resources resources) {
+        String timeStamp = new SimpleDateFormat(resources.getString(R.string.date_format)).format(new Date());
+        String imageNamePrefix = resources.getString(R.string.image_name_prefix);
+        return imageNamePrefix + timeStamp;
+    }
+
+    public static File createImageFileInAppGalleryDirectory(Context context, String
+            imageFileName, String mimeType) throws IOException {
+        Resources resources = context.getResources();
+        String imageNameSuffix = resources.getString(R.string.image_name_suffix, MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType));
+        File appGalleryImagesDirectory = FileUtils.getExternalPictureStorageDirectory(resources, resources.getString(R.string.nebo_album));
+        Log.e("Nebo", Thread.currentThread().getStackTrace()[2] + "appGalleryImagesDirectory" + appGalleryImagesDirectory);
+        return File.createTempFile(imageFileName, imageNameSuffix, appGalleryImagesDirectory);
+    }
+
+    public static File createImageFileInAppCacheDirectory(Context context, String imageFileName, String mimeType) throws IOException {
+        String imageNameSuffix = context.getResources().getString(R.string.image_name_suffix, MimeTypeMap.getSingleton
+                ().getExtensionFromMimeType(mimeType));
+        File appImagesDirectory = context.getCacheDir();
+        return File.createTempFile(imageFileName, imageNameSuffix, appImagesDirectory);
+    }
+
+    public static File scaleToImageViewSize(File imageFile, int targetW, int targetH) {
+                /*This function has been entiery copied from Google Sample PhotoIntentActivity https://developer.android.com/training/camera/photobasics.html*/
+        /* There isn't enough memory to open up more than a couple camera photos */
+        /* So pre-scale the target bitmap into which the file is decoded */
+
+        String imagePath = imageFile.getPath();
+
+        /* Get the size of the image */
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        /* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+        }
+
+        /* Set bitmap options to scale the image decode target */
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        /* Decode the JPEG file into a Bitmap */
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        return imageFile;
+    }
+
+    public static File saveBitmapInFile(Bitmap bitmap, File file, String mimetype) {
+        OutputStream outStream = null;
+        try {
+            outStream = new FileOutputStream(file);
+            bitmap.compress(mimetype.equals("image/jpeg") ? Bitmap.CompressFormat.JPEG : Bitmap.CompressFormat.PNG, 100, outStream);
+            outStream.flush();
+            outStream.close();
+            return file;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
